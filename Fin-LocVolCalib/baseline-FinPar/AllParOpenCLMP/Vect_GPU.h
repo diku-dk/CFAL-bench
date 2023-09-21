d85d0abf19e50d1ace064417cd6d0e28e4d02519
Cosmin Oancea <cosmin.oancea@diku.dk>
2023-09-21 23:34:25 +0200

    minor

--- Fin-LocVolCalib/baseline-FinPar/AllParOpenCLMP/Vect_GPU.h
+++ Fin-LocVolCalib/baseline-FinPar/AllParOpenCLMP/Vect_GPU.h
@@ -162,6 +162,8 @@ runOnGPU ( RWScalars& ro_scal, NordeaArrays& cpu_arrs, oclNordeaArrays& ocl_arrs
             run_GPUkernels_one_time_iteration ( cqCommandQueue[dev_id], kernels );
     } // END TIME LOOP!
 
+    clFinish(cqCommandQueue[dev_id]);
+
     gettimeofday(&t_end, NULL);
     timeval_subtract(&t_diff, &t_end, &t_start);
     elapsed = t_diff.tv_sec*1e6+t_diff.tv_usec;
