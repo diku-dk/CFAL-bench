9100ec2e18a644365452c4188b6c4605053e3353
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2024-02-02 09:50:55 +0100

    Added standard and flash attention algorithms from paper.

--- FlashAttention/dace/flash_attention_naive.py
+++ FlashAttention/dace/flash_attention_naive.py
@@ -59,6 +59,65 @@ def FlashAttention(Q, K, V, O, P_block, d, N):
         O[i * d:(i + 1) * d, :] = O_i
 
 
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=1, keepdims=True)
+
+
+def standard_attention(Q, K, V):
+
+    N, d = Q.shape
+
+    S = Q @ K.T
+    P = softmax(S)
+    O = P @ V
+
+    return O
+
+
+def flash_attention(Q, K, V, M):
+
+    N, d = Q.shape
+    Bc = int(np.ceil(M / (4 * d)))
+    Br = min(Bc, d)
+    Tc = int(np.ceil(N / Bc))
+    Tr = int(np.ceil(N / Br))
+
+    O = np.zeros((N, d), dtype=Q.dtype)
+    l = np.zeros((N,), dtype=Q.dtype)
+    m = np.full((N,), -np.inf, dtype=Q.dtype)
+
+    for j in range(Tc):
+
+        Kj = K[j * Bc:min((j + 1) * Bc, N), :]
+        Vj = V[j * Bc:min((j + 1) * Bc, N), :]
+
+        for i in range(Tr):
+
+            Qi = Q[i * Br:min((i + 1) * Br, N), :]
+            Oi = O[i * Br:min((i + 1) * Br, N), :]
+            li = l[i * Br:min((i + 1) * Br, N)]
+            mi = m[i * Br:min((i + 1) * Br, N)]
+
+            Sij = Qi @ Kj.T
+            mij = np.max(Sij, axis=1)
+            Pij = np.exp(Sij - mij.reshape(len(mij), 1)) 
+            lij = np.sum(Pij, axis=1)
+
+            mi_new = np.maximum(mi, mij)
+            li_new = np.exp(mi - mi_new) * li + np.exp(mij - mi_new) * lij
+
+            # Oi[:] = np.diag(1 / li_new) @ (np.diag(li) @ np.exp(mi - mi_new).reshape((len(mi), 1)) * Oi + np.exp(mij - mi_new).reshape((len(mij), 1)) * Pij @ Vj)
+            Oi[:] = (
+                    (li.reshape(len(li), 1) * np.exp(mi - mi_new).reshape((len(mi), 1))) * Oi +
+                    np.exp(mij - mi_new).reshape((len(mij), 1)) * Pij @ Vj
+                ) / li_new.reshape(len(li_new), 1)
+            li[:] = li_new
+            mi[:] = mi_new
+
+    return O
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -72,12 +131,32 @@ if __name__ == "__main__":
 
     # Create data. The input sequences (Q, K, and V) and output one (O) are Nxd matrices,
     # P_block is d x N
-    Q = np.ones((N, d)).astype(np.float32)
-    K = np.ones((N, d)).astype(np.float32)
-    V = np.ones((N, d)).astype(np.float32)
+    rng = np.random.default_rng(42)
+    # Q = np.ones((N, d)).astype(np.float32)
+    # K = np.ones((N, d)).astype(np.float32)
+    # V = np.ones((N, d)).astype(np.float32)
+    Q = rng.random((N, d)).astype(np.float32)
+    K = rng.random((N, d)).astype(np.float32)
+    V = rng.random((N, d)).astype(np.float32)
 
+    O_ref = np.empty((N, d)).astype(np.float32)
     O = np.empty((N, d)).astype(np.float32)
     P_block = np.empty((d, N)).astype(np.float32)
+
+    ### Start
+    start = time.time()
+    O_ref = standard_attention(Q, K, V)
+    end = time.time()
+
+    ### Check
+
+    # assert np.allclose(np.linalg.norm(O), math.sqrt(N * d)), f"L2 norm is {l2(O)}, it should be {math.sqrt(N*d)}"
+
+    ### Perf
+    print(f"Time in usecs: {(end-start)*1e6}")
+    duration = end - start
+    ops = 2.0 * N * N * (d + 1)
+    print(f"Compute rate: {ops/duration/1e9} Gflops/s\n")
 
     ### Start
     start = time.time()
@@ -86,10 +165,33 @@ if __name__ == "__main__":
 
     ### Check
 
-    assert np.allclose(np.linalg.norm(O), math.sqrt(N * d)), f"L2 norm is {l2(O)}, it should be {math.sqrt(N*d)}"
+    # assert np.allclose(np.linalg.norm(O), math.sqrt(N * d)), f"L2 norm is {l2(O)}, it should be {math.sqrt(N*d)}"
+    # assert np.allclose(O, O_ref)
+    print(np.linalg.norm(O - O_ref) / np.linalg.norm(O_ref))
 
     ### Perf
     print(f"Time in usecs: {(end-start)*1e6}")
     duration = end - start
     ops = 2.0 * N * N * (d + 1)
     print(f"Compute rate: {ops/duration/1e9} Gflops/s\n")
+
+    M0 = 4 * N * d
+    for M in (M0, M0 // 2, M0 // 4, M0 // 8, M0 // 16, M0 // 32):
+        print(f"M = {M}")
+
+        ### Start
+        start = time.time()
+        O = flash_attention(Q, K, V, M)
+        end = time.time()
+
+        ### Check
+
+        # assert np.allclose(np.linalg.norm(O), math.sqrt(N * d)), f"L2 norm is {l2(O)}, it should be {math.sqrt(N*d)}"
+        # assert np.allclose(O, O_ref)
+        print(np.linalg.norm(O - O_ref) / np.linalg.norm(O_ref))
+
+        ### Perf
+        print(f"Time in usecs: {(end-start)*1e6}")
+        duration = end - start
+        ops = 2.0 * N * N * (d + 1)
+        print(f"Compute rate: {ops/duration/1e9} Gflops/s\n")
