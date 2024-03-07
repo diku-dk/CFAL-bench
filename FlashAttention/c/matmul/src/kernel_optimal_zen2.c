1b702a32d1adcf4f590dedbd644c27f15ebd4176
thomas <thomas.koopman@ru.nl>
2024-03-07 14:53:13 +0100

    Add different kernel

--- FlashAttention/c/matmul/src/kernel_optimal_zen2.c
+++ FlashAttention/c/matmul/src/kernel_optimal_zen2.c
@@ -16,19 +16,22 @@
  * That means reusing each element 4 times suffices to bridge the
  * L2 -> register bandwidth.
  *
- * For zen2, 4.0 GHz: 90 Gflops/s for k variable. If statically known,
- * this becomes 100 Gflops/s.
+ * Similar to kernel.c, but C is less square. This is better for the
+ * microkernel as we need less broadcasts, but the reuse is less,
+ * so this may give problems in a full implementation.
  *
- * Peak performance is 118 Gflops/s, so 20% is left on the table.
+ * For zen2, 4.0 GHz: 124 Gflops/s
+ *
+ * Peak performance is 128 Gflops/s, so close enough. k does not need
+ * to be statically known here.
  **/
 
 #include <immintrin.h>
 #include <cblas.h>
 #include <sys/time.h>
 
-#define MR 12
-#define NR 8
-#define KC 128
+#define MR 6
+#define NR 16
 
 float maxf(float a, float b)
 {
@@ -40,71 +43,63 @@ float maxf(float a, float b)
 inline
 void kernel(float *a, float *b, float *c, int k, int ld_c)
 {
-    k = KC;
+    __m256 c00  = _mm256_loadu_ps(&c[0 * ld_c + 0]);
+    __m256 c01  = _mm256_loadu_ps(&c[0 * ld_c + 8]);
+    __m256 c10  = _mm256_loadu_ps(&c[1 * ld_c + 0]);
+    __m256 c11  = _mm256_loadu_ps(&c[1 * ld_c + 8]);
+    __m256 c20  = _mm256_loadu_ps(&c[2 * ld_c + 0]);
+    __m256 c21  = _mm256_loadu_ps(&c[2 * ld_c + 8]);
+    __m256 c30  = _mm256_loadu_ps(&c[3 * ld_c + 0]);
+    __m256 c31  = _mm256_loadu_ps(&c[3 * ld_c + 8]);
+    __m256 c40  = _mm256_loadu_ps(&c[4 * ld_c + 0]);
+    __m256 c41  = _mm256_loadu_ps(&c[4 * ld_c + 8]);
+    __m256 c50  = _mm256_loadu_ps(&c[5 * ld_c + 0]);
+    __m256 c51  = _mm256_loadu_ps(&c[5 * ld_c + 8]);
 
-    __m256 c0  = _mm256_loadu_ps(&c[0  * ld_c]);
-    __m256 c1  = _mm256_loadu_ps(&c[1  * ld_c]);
-    __m256 c2  = _mm256_loadu_ps(&c[2  * ld_c]);
-    __m256 c3  = _mm256_loadu_ps(&c[3  * ld_c]);
-    __m256 c4  = _mm256_loadu_ps(&c[4  * ld_c]);
-    __m256 c5  = _mm256_loadu_ps(&c[5  * ld_c]);
-    __m256 c6  = _mm256_loadu_ps(&c[6  * ld_c]);
-    __m256 c7  = _mm256_loadu_ps(&c[7  * ld_c]);
-    __m256 c8  = _mm256_loadu_ps(&c[8  * ld_c]);
-    __m256 c9  = _mm256_loadu_ps(&c[9  * ld_c]);
-    __m256 c10 = _mm256_loadu_ps(&c[10 * ld_c]);
-    __m256 c11 = _mm256_loadu_ps(&c[11 * ld_c]);
-
-    /* Use two registers for pipelining */
     __m256 ai;  /* a[i, p]   */
-    __m256 ai2; /* a[i, p]   */
-    __m256 bp;  /* b[p, 0:7] */
+    __m256 bp1; /* b[p, 0:7] */
+    __m256 bp2; /* b[p, 8:15] */
     for (int p = 0; p < k; p++) {
-        bp = _mm256_loadu_ps(&b[p * NR]);
+        bp1 = _mm256_loadu_ps(&b[p * NR]);
+        bp2 = _mm256_loadu_ps(&b[p * NR + 8]);
 
-        ai    = _mm256_broadcast_ss(&a[0 * k + p]);
-        ai2   = _mm256_broadcast_ss(&a[1 * k + p]);
-        c0    = _mm256_fmadd_ps(ai, bp, c0);
-        c1    = _mm256_fmadd_ps(ai2, bp, c1);
+        ai  = _mm256_broadcast_ss(&a[0 * k + p]);
+        c00 = _mm256_fmadd_ps(ai, bp1, c00);
+        c01 = _mm256_fmadd_ps(ai, bp2, c01);
 
-        ai    = _mm256_broadcast_ss(&a[2 * k + p]);
-        ai2   = _mm256_broadcast_ss(&a[3 * k + p]);
-        c2    = _mm256_fmadd_ps(ai, bp, c2);
-        c3    = _mm256_fmadd_ps(ai2, bp, c3);
+        ai  = _mm256_broadcast_ss(&a[1 * k + p]);
+        c10 = _mm256_fmadd_ps(ai, bp1, c10);
+        c11 = _mm256_fmadd_ps(ai, bp2, c11);
 
-        ai    = _mm256_broadcast_ss(&a[4 * k + p]);
-        ai2   = _mm256_broadcast_ss(&a[5 * k + p]);
-        c4    = _mm256_fmadd_ps(ai, bp, c4);
-        c5    = _mm256_fmadd_ps(ai2, bp, c5);
+        ai  = _mm256_broadcast_ss(&a[2 * k + p]);
+        c20 = _mm256_fmadd_ps(ai, bp1, c20);
+        c21 = _mm256_fmadd_ps(ai, bp2, c21);
 
-        ai    = _mm256_broadcast_ss(&a[6 * k + p]);
-        ai2   = _mm256_broadcast_ss(&a[7 * k + p]);
-        c6    = _mm256_fmadd_ps(ai, bp, c6);
-        c7    = _mm256_fmadd_ps(ai2, bp, c7);
+        ai  = _mm256_broadcast_ss(&a[3 * k + p]);
+        c30 = _mm256_fmadd_ps(ai, bp1, c30);
+        c31 = _mm256_fmadd_ps(ai, bp2, c31);
 
-        ai    = _mm256_broadcast_ss(&a[8 * k + p]);
-        ai2   = _mm256_broadcast_ss(&a[9 * k + p]);
-        c8    = _mm256_fmadd_ps(ai, bp, c8);
-        c9    = _mm256_fmadd_ps(ai2, bp, c9);
+        ai  = _mm256_broadcast_ss(&a[4 * k + p]);
+        c40 = _mm256_fmadd_ps(ai, bp1, c40);
+        c41 = _mm256_fmadd_ps(ai, bp2, c41);
 
-        ai    = _mm256_broadcast_ss(&a[10 * k + p]);
-        ai2   = _mm256_broadcast_ss(&a[11 * k + p]);
-        c10   = _mm256_fmadd_ps(ai, bp, c10);
-        c11   = _mm256_fmadd_ps(ai2, bp, c11);
+        ai  = _mm256_broadcast_ss(&a[5 * k + p]);
+        c50 = _mm256_fmadd_ps(ai, bp1, c50);
+        c51 = _mm256_fmadd_ps(ai, bp2, c51);
     }
 
-    _mm256_storeu_ps(&c[0  * ld_c], c0);
-    _mm256_storeu_ps(&c[1  * ld_c], c1);
-    _mm256_storeu_ps(&c[2  * ld_c], c2);
-    _mm256_storeu_ps(&c[3  * ld_c], c3);
-    _mm256_storeu_ps(&c[4  * ld_c], c4);
-    _mm256_storeu_ps(&c[5  * ld_c], c5);
-    _mm256_storeu_ps(&c[6  * ld_c], c6);
-    _mm256_storeu_ps(&c[7  * ld_c], c7);
-    _mm256_storeu_ps(&c[8  * ld_c], c8);
-    _mm256_storeu_ps(&c[9  * ld_c], c9);
-    _mm256_storeu_ps(&c[10 * ld_c], c10);
-    _mm256_storeu_ps(&c[11 * ld_c], c11);
+    _mm256_storeu_ps(&c[0 * ld_c + 0], c00);
+    _mm256_storeu_ps(&c[0 * ld_c + 8], c01);
+    _mm256_storeu_ps(&c[1 * ld_c + 0], c10);
+    _mm256_storeu_ps(&c[1 * ld_c + 8], c11);
+    _mm256_storeu_ps(&c[2 * ld_c + 0], c20);
+    _mm256_storeu_ps(&c[2 * ld_c + 8], c21);
+    _mm256_storeu_ps(&c[3 * ld_c + 0], c30);
+    _mm256_storeu_ps(&c[3 * ld_c + 8], c31);
+    _mm256_storeu_ps(&c[4 * ld_c + 0], c40);
+    _mm256_storeu_ps(&c[4 * ld_c + 8], c41);
+    _mm256_storeu_ps(&c[5 * ld_c + 0], c50);
+    _mm256_storeu_ps(&c[5 * ld_c + 8], c51);
 }
 
 float test_kernel(int k)
