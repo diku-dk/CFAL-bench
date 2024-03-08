8b8a45779e05f4b1bfb49b0241a37f6cbed2a88e
thomas <thomas.koopman@ru.nl>
2024-03-08 10:41:56 +0100

    Add multithreaded version

--- FlashAttention/c/test.sh
+++ FlashAttention/c/test.sh
@@ -18,25 +18,30 @@ flasho=$(mktemp)
 alg1o=$(mktemp)
 blas_alg1o=$(mktemp)
 custom_alg1o=$(mktemp)
+custom_alg1o_mt=$(mktemp)
 
 make -j
 
 bin/generate "$d" "$N" -random > "$inputf"
-bin/attention -io < "$inputf" > "$attentiono"
 
-bin/flash -io < "$inputf" > "$flasho"
-bin/flash_alg1 "$M" -io < "$inputf" > "$alg1o"
-bin/blas_alg1 "$M" -io < "$inputf" > "$blas_alg1o"
-bin/custom_alg1 "$M" -io < "$inputf" > "$custom_alg1o"
+OMP_NUM_THREADS=1 bin/attention -io < "$inputf" > "$attentiono"
 
 printf "Attention vs flash\n"
+OMP_NUM_THREADS=1 bin/flash -io < "$inputf" > "$flasho"
 bin/compare "$d" "$M" "$attentiono" "$flasho"
 
 printf "Attention vs flash_alg1\n"
+bin/flash_alg1 "$M" -io < "$inputf" > "$alg1o"
 bin/compare "$d" "$M" "$attentiono" "$alg1o"
 
 printf "Attention vs blas_alg1\n"
+OMP_NUM_THREADS=1 bin/blas_alg1 "$M" -io < "$inputf" > "$blas_alg1o"
 bin/compare "$d" "$M" "$attentiono" "$blas_alg1o"
 
 printf "Attention vs custom_alg1\n"
+bin/custom_alg1 "$M" -io < "$inputf" > "$custom_alg1o"
 bin/compare "$d" "$M" "$attentiono" "$custom_alg1o"
+
+printf "Attention vs custom_alg1_mt\n"
+OMP_NUM_THREADS=4 bin/custom_alg1_mt "$M" -io < "$inputf" > "$custom_alg1o_mt"
+bin/compare "$d" "$M" "$attentiono" "$custom_alg1o_mt"
