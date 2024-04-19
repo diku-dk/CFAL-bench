dae40ee645fc049581f1bf553beedbf5d8677195
David <d.p.vanbalen@uu.nl>
2024-04-19 21:24:21 +0200

    first iteration on flashattention

--- FlashAttention/accelerate/src/Flash_alg1.hs
+++ FlashAttention/accelerate/src/Flash_alg1.hs
@@ -6,7 +6,8 @@ module Flash_alg1 where
 import Data.Array.Accelerate
 import Prelude hiding (replicate, zipWith, zipWith3, map, sum, min, Ord(..), maximum)
 
--- wip: port of flash_alg1.sac to accelerate
+-- untested!
+-- direct port of flash_alg1.sac to accelerate
 
 -- m is for blocking
 flashAttention :: Acc (Matrix Float) -> Acc (Matrix Float) -> Acc (Matrix Float) -> Exp Int -> Acc (Matrix Float)
@@ -15,18 +16,18 @@ flashAttention q k v m' =
       bc = ceildiv m' (4*d)
       br = min bc d
 
-      qb = reshape (Z_ ::. n / br ::. br ::. d) q
-      kb = reshape (Z_ ::. n / bc ::. bc ::. d) k
-      vb = reshape (Z_ ::. n / bc ::. bc ::. d) v
+      qb = reshape (Z_ ::. n `div` br ::. br ::. d) q
+      kb = reshape (Z_ ::. n `div` bc ::. bc ::. d) k
+      vb = reshape (Z_ ::. n `div` bc ::. bc ::. d) v
 
-      o = fill (Z_ ::. n / br ::. br ::. d) 0
-      m = fill (Z_ ::. n / br ::. br)       (negate real_max)
-      l = fill (Z_ ::. n / br ::. br)       0
+      o = fill (Z_ ::. n `div` br ::. br ::. d) 0
+      m = fill (Z_ ::. n `div` br ::. br)       (negate real_max)
+      l = fill (Z_ ::. n `div` br ::. br)       0
 
-      max_j = n / bc
+      max_j = n `div` bc
       (T3 result _ _) = afst $ awhile (map (< max_j) . asnd)
-             (\(T2 state j) -> T2 (step state qb kb vb j) (j+1))
-             (T2 (T3 o m l) 0)
+             (\(T2 state j) -> T2 (step state qb kb vb j) (map (+1) j))
+             (T2 (T3 o m l) $ unit 0)
   in reshape (Z_ ::. n ::. d) result
 
 real_max = 3.40282346638528859811704183484516925e+38
@@ -36,26 +37,28 @@ type State = Acc (Array DIM3 Float,Matrix Float,Matrix Float)
 step :: State -> Acc (Array DIM3 Float) -> Acc (Array DIM3 Float) -> Acc (Array DIM3 Float) -> Acc (Scalar Int) -> State
 step (T3 o m l) qb kb vb j =
   let Z_ ::. nbc ::. bc ::. d = shape kb
-      kb' = replicate (Z_ ::. nbc ::. All_ ::. All_) $ slice kb $ Z_ ::. the j ::. All_ ::. All_
-      T3 pj1 mj lj = exp_e $ matmulT qb kb'
+      Z_ ::. nbr ::. br ::. _d = shape o
+      kbj = replicate (Z_ ::. nbc ::. All_ ::. All_) $ slice kb $ Z_ ::. the j ::. All_ ::. All_
+      vbj = replicate (Z_ ::. nbr ::. All_ ::. All_) $ slice kb $ Z_ ::. the j ::. All_ ::. All_
+      T3 pj1 mj lj = exp_e $ matmulT qb kbj
       mnew = zipWith max m mj
       lnew = zipWith5 (\m_ mnew_ l_ mj_ lj_ -> exp (m_-mnew_) * l_ + exp (mj_ - mnew_) * lj_)
                         m  mnew  l  mj  lj
       o' = zipWith4 (\l_ m_ mnew_ o_ -> l_ * exp (m_ - mnew_) * o_)
-            (replicate _ l)
-            (replicate _ m)
-            (replicate _ mnew)
+            (replicate (Any_ ::. d) l)
+            (replicate (Any_ ::. d) m)
+            (replicate (Any_ ::. d) mnew)
             o
       pj2 = zipWith3 (\mj' mnew' pj1' -> exp (mj' - mnew')*pj1')
-              (replicate (Z_ ::. All_ ::. All_ ::. d) mj) 
-              (replicate (Z_ ::. All_ ::. All_ ::. d) mnew) 
+              (replicate (Any_ ::. d) mj) 
+              (replicate (Any_ ::. d) mnew) 
               pj1
-      o'' = undefined o' pj2 vbj
-      o''' = zipWith (/) o (replicate _ lnew)
+      o'' = zipWith (+) o' $ matmul pj2 vbj
+      o''' = zipWith (/) o (replicate (Any_ ::. d) lnew)
   in T3 o''' mnew lnew
 
 ceildiv :: Exp Int -> Exp Int -> Exp Int
-ceildiv a b = (a + b - 1) / b
+ceildiv a b = (a + b - 1) `div` b
 
 exp_e :: Shape sh => Acc (Array (sh :. Int) Float) -> Acc (Array (sh :. Int) Float, Array sh Float, Array sh Float)
 exp_e x =
