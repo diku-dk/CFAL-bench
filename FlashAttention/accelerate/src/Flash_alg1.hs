57f34f4731c1eec61fb7783583d486e9858fe7fa
David <d.p.vanbalen@uu.nl>
2024-05-01 17:50:37 +0200

    accelerate-flashattention fix+correctness check+benchmark

--- FlashAttention/accelerate/src/Flash_alg1.hs
+++ FlashAttention/accelerate/src/Flash_alg1.hs
@@ -1,15 +1,22 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 module Flash_alg1 where
 
-import Data.Array.Accelerate
+import Data.Array.Accelerate hiding (encodeFloat,(^))
 import Prelude hiding (replicate, zipWith, zipWith3, map, sum, min, Ord(..), maximum)
+import Data.Array.Accelerate.Debug.Trace
 
 -- untested!
 -- direct port of flash_alg1.sac to accelerate
+totalProgram :: Acc (Scalar Int, Scalar Int, Scalar Int) -> Acc (Matrix Float)
+totalProgram (T3 n d m) = let T3 q k v = mkInput (the n) (the d) in flashAttention q k v (the m)
 
--- m is for blocking
+check :: Acc (Scalar Int, Scalar Int, Scalar Int) -> Acc (Scalar Float, Scalar Float)
+check a@(T3 x _ _ ) = checkCorrectness . atraceArray "done" x . totalProgram $ a
+
+
 flashAttention :: Acc (Matrix Float) -> Acc (Matrix Float) -> Acc (Matrix Float) -> Exp Int -> Acc (Matrix Float)
 flashAttention q k v m' =
   let Z_ ::. n ::. d = shape q
@@ -25,12 +32,15 @@ flashAttention q k v m' =
       l = fill (Z_ ::. n `div` br ::. br)       0
 
       max_j = n `div` bc
-      (T3 result _ _) = afst $ awhile (map (< max_j) . asnd)
+      x@(T3 result _ _) = afst $ awhile (map (< max_j) . asnd)
              (\(T2 state j) -> T2 (step state qb kb vb j) (map (+1) j))
              (T2 (T3 o m l) $ unit 0)
-  in reshape (Z_ ::. n ::. d) result
+  in atraceExp "reshape" (shape result) $ reshape (Z_ ::. n ::. d) $ atrace "result" result
 
-real_max = 3.40282346638528859811704183484516925e+38
+-- awhile' :: (p2 -> p3) -> (p2 -> p2) -> p2 -> p2
+-- awhile' _ s x = let y = s . s . s $ x in y
+
+real_max = constant $ encodeFloat (2^(24 :: Int) - 1) (127-23)--3.40282346638528859811704183484516925e+38, largest non-infinity float
 
 type State = Acc (Array DIM3 Float,Matrix Float,Matrix Float)
 
@@ -54,8 +64,8 @@ step (T3 o m l) qb kb vb j =
               (replicate (Any_ ::. d) mnew) 
               pj1
       o'' = zipWith (+) o' $ matmul pj2 vbj
-      o''' = zipWith (/) o (replicate (Any_ ::. d) lnew)
-  in T3 o''' mnew lnew
+      o''' = zipWith (/) o'' (replicate (Any_ ::. d) lnew)
+  in atraceArray "o'''" j $ T3 o''' mnew lnew
 
 ceildiv :: Exp Int -> Exp Int -> Exp Int
 ceildiv a b = (a + b - 1) `div` b
@@ -83,4 +93,16 @@ transpose' :: (Shape sh, Elt a) => Acc (Array (sh :. Int:.Int) a) -> Acc (Array 
 transpose' x =
   let sh ::. a ::. b = shape x
   in backpermute (sh ::. b ::. a) (\(sh ::. b ::. a) -> sh ::. a ::. b) x
+
+mkInput :: Exp Int -> Exp Int -> Acc (Matrix Float, Matrix Float, Matrix Float)
+mkInput n d = T3 
+  (generate (Z_ ::. n ::. d) (const 1.0)) 
+  (generate (Z_ ::. n ::. d) (const 1.0)) 
+  (generate (Z_ ::. n ::. d) (const 1.0))
+
+checkCorrectness :: Acc (Matrix Float) -> Acc (Scalar Float, Scalar Float)
+checkCorrectness o = let Z_ ::. n ::. d = shape o
+                         target = sqrt (toFloating n * toFloating d)
+                         result = map sqrt $ sum $ sum $ map (\x -> x*x) o
+                     in T2 (unit target) result
 
