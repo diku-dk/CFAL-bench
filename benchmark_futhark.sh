71a12e1fae14830ce3d0cb19ca207fa57ed15a35
Troels Henriksen <athas@sigkill.dk>
2024-05-14 19:16:35 +0200

    Neater.

--- benchmark_futhark.sh
+++ benchmark_futhark.sh
@@ -13,15 +13,10 @@ make -C MG/futhark run_multicore
 make -C MG/futhark run_cuda
 
 echo MG CPU GFLOP/s
-./futhark-time2flops.py MG/futhark/mg_multicore.json mg.fut:mgNAS 'Class A' 3.625
-./futhark-time2flops.py mg/futhark/mg_multicore.json mg.fut:mgNAS 'Class B' 18.125
-./futhark-time2flops.py mg/futhark/mg_multicore.json mg.fut:mgNAS 'Class C' 145.0
+./futhark-time2flops.py MG/futhark/mg_multicore.json mg.fut:mgNAS 'Class A' 3.625 'Class B' 18.125  'Class C' 145.0
 
 echo MG GPU GFLOP/s
-echo MG CPU GFLOP/s
-./futhark-time2flops.py mg/futhark/mg_cuda.json mg.fut:mgNAS 'Class A' 3.625
-./futhark-time2flops.py mg/futhark/mg_cuda.json mg.fut:mgNAS 'Class B' 18.125
-./futhark-time2flops.py mg/futhark/mg_cuda.json mg.fut:mgNAS 'Class C' 145.0
+./futhark-time2flops.py MG/futhark/mg_cuda.json mg.fut:mgNAS 'Class A' 3.625  'Class B' 18.125 'Class C' 145.0
 
 make -C LocVolCalib/futhark run_ispc
 make -C LocVolCalib/futhark run_cuda
@@ -33,11 +28,7 @@ make -C nbody-naive/futhark run_multicore
 make -C nbody-naive/futhark run_cuda
 
 echo N-body CPU GFLOP/s
-./futhark-time2flops.py nbody-naive/futhark/nbody_multicore.json nbody.fut 'k=10, n=1000' 0.18012
-./futhark-time2flops.py nbody-naive/futhark/nbody_multicore.json nbody.fut 'k=10, n=10000' 18.0012
-./futhark-time2flops.py nbody-naive/futhark/nbody_multicore.json nbody.fut 'k=10, n=100000' 1800.0012
+./futhark-time2flops.py nbody-naive/futhark/nbody_multicore.json nbody.fut 'k=10, n=1000' 0.18012 'k=10, n=10000' 18.0012 'k=10, n=100000' 1800.0012
 
 echo N-body GPU GFLOP/s
-./futhark-time2flops.py nbody-naive/futhark/nbody_cuda.json nbody.fut 'k=10, n=1000' 0.18012
-./futhark-time2flops.py nbody-naive/futhark/nbody_cuda.json nbody.fut 'k=10, n=10000' 18.0012
-./futhark-time2flops.py nbody-naive/futhark/nbody_cuda.json nbody.fut 'k=10, n=100000' 1800.0012
+./futhark-time2flops.py nbody-naive/futhark/nbody_cuda.json nbody.fut 'k=10, n=1000' 0.18012 'k=10, n=10000' 18.0012 'k=10, n=100000' 1800.0012
