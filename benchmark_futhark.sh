4623e4a53c6bb50dd6ffd309cf9e7c4c6b83ed18
Troels Henriksen <athas@sigkill.dk>
2024-05-14 16:30:02 +0200

    Helpful comment.

--- benchmark_futhark.sh
+++ benchmark_futhark.sh
@@ -3,7 +3,9 @@
 # Run all Futhark benchmarks. Assumes 'futhark' is on PATH and is
 # operational with the 'cuda', 'multicore', and 'ispc' backends.
 #
-# You can submit this script as a slurm job.
+# You can submit this script as a slurm job. On the shared system, do:
+#
+# $ srun -t 10:00 -p csmpi_fpga_short -c 32 --gres=gpu:nvidia_a30:1 ./benchmark_futhark.sh
 
 make -C LocVolCalib/futhark run_ispc
 make -C LocVolCalib/futhark run_cuda
