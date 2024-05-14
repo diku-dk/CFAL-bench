d5c3379f54019bf2beace9d8ef68124451546b66
Troels Henriksen <athas@sigkill.dk>
2024-05-14 18:13:32 +0200

    Like so.

--- benchmark_futhark.sh
+++ benchmark_futhark.sh
@@ -18,3 +18,13 @@ make -C quickhull/futhark run_cuda
 
 make -C nbody-naive/futhark run_multicore
 make -C nbody-naive/futhark run_cuda
+
+echo N-body CPU GFLOP/s
+./futhark-time2flops.py nbody-naive/futhark/nbody_multicore.json nbody.fut 'k=10, n=1000' 0.18012
+./futhark-time2flops.py nbody-naive/futhark/nbody_multicore.json nbody.fut 'k=10, n=1000' 18.0012
+./futhark-time2flops.py nbody-naive/futhark/nbody_multicore.json nbody.fut 'k=10, n=1000' 1800.0012
+
+echo N-body GPU GFLOP/s
+./futhark-time2flops.py nbody-naive/futhark/nbody_cuda.json nbody.fut 'k=10, n=1000' 0.18012
+./futhark-time2flops.py nbody-naive/futhark/nbody_cuda.json nbody.fut 'k=10, n=1000' 18.0012
+./futhark-time2flops.py nbody-naive/futhark/nbody_cuda.json nbody.fut 'k=10, n=1000' 1800.0012
