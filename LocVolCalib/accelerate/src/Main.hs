60ff674befcf96a82e705e4288e552fa2e138f60
David van Balen <dpvanbalen@hotmail.com>
2024-07-08 17:21:56 +0200

    quickhull-accelerate: read new input. locvolcalib-accelerate: add benchmarking

--- LocVolCalib/accelerate/src/Main.hs
+++ LocVolCalib/accelerate/src/Main.hs
@@ -6,31 +6,69 @@ import Criterion
 import Data.Array.Accelerate
 import qualified Data.Array.Accelerate.Interpreter as Interp
 import qualified Data.Array.Accelerate.LLVM.Native as CPU
+import qualified Data.Array.Accelerate.LLVM.PTX as GPU
 import Prelude hiding (sum)
 
 -- currently does NOT work: returns only NaNs. Not sure why, it seems like a faithful translation from Futhark.
 main :: IO ()
-main = do
+main = 
+  defaultMain [backend "CPU" CPU.runN, backend "GPU" GPU.runN]
+
+    where
+      backend name run = bgroup name $ map benchrun run [small, medium, large]
+      benchrun run (name, input) = bench name $ nf (run main') input
+
+      small = ("small", T9
+                        (constant 16)
+                        (constant 32)
+                        (constant 256)
+                        (constant 256)
+                        (constant 0.03)
+                        (constant 5.0)
+                        (constant 0.2)
+                        (constant 0.6)
+                        (constant 0.5))
+      medium = ("medium", T9 
+                        (constant 128 )
+                        (constant 256 )
+                        (constant 32  )
+                        (constant 64  )
+                        (constant 0.03)
+                        (constant 5.0 )
+                        (constant 0.2 )
+                        (constant 0.6 )
+                        (constant 0.5 ))
+      large = ("large", T9 
+                        (constant 256 )
+                        (constant 256 )
+                        (constant 256 )
+                        (constant 64  )
+                        (constant 0.03)
+                        (constant 5.0 )
+                        (constant 0.2 )
+                        (constant 0.6 )
+                        (constant 0.5 ))
+
   print $ CPU.runN $ -- const (sum $ generate (Z_ ::. (constant 40 :: Exp Int)) (const (constant 40 :: Exp Int))) $
     main' $ T9 
         -- small
-      (constant 16)
-      (constant 32)
-      (constant 256)
-      (constant 256)
-      (constant 0.03)
-      (constant 5.0)
-      (constant 0.2)
-      (constant 0.6)
-      (constant 0.5)
+      -- (constant 16)
+      -- (constant 32)
+      -- (constant 256)
+      -- (constant 256)
+      -- (constant 0.03)
+      -- (constant 5.0)
+      -- (constant 0.2)
+      -- (constant 0.6)
+      -- (constant 0.5)
 
         -- medium
-      -- (constant 128 )
-      -- (constant 256 )
-      -- (constant 32  )
-      -- (constant 64  )
-      -- (constant 0.03)
-      -- (constant 5.0 )
-      -- (constant 0.2 )
-      -- (constant 0.6 )
-      -- (constant 0.5 )
+      (constant 128 )
+      (constant 256 )
+      (constant 32  )
+      (constant 64  )
+      (constant 0.03)
+      (constant 5.0 )
+      (constant 0.2 )
+      (constant 0.6 )
+      (constant 0.5 )
--- quickhull/accelerate/src/Main.hs
+++ quickhull/accelerate/src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 module Main where
 import qualified Data.Array.Accelerate as A
 import qualified Data.Array.Accelerate.LLVM.Native as CPU
@@ -6,10 +8,16 @@ import Criterion
 import Criterion.Main
 
 import Quickhull
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Internal as BI
+import GHC.IsList
+import Data.Int
+import Foreign.ForeignPtr (ForeignPtr, castForeignPtr)
+import qualified Data.Array.Accelerate.IO.Foreign.ForeignPtr as A
 
 main :: IO ()
 main = do
-  inputs <- mapM load ["1M_rectangle_16384", "1M_circle_16384", "1M_quadratic_2147483648"]
+  inputs <- mapM load ["1M_rectangle", "1M_circle", "1M_quadratic", "100M_rectangle", "100M_circle", "100M_quadratic"]
 
   let quickhullCPU = CPU.runN quickhull
   let quickhullGPU = GPU.runN quickhull
@@ -30,17 +38,19 @@ type Input = (String, A.Vector Point)
 load :: String -> IO Input
 load name = do
   putStrLn $ "Loading " ++ name
-  content <- readFile $ "../input/" ++ name ++ ".dat"
-  let list = map parseLine $ lines content
-  list `seq` return (name, A.fromList (A.Z A.:. length list) list)
-  where
-    parseLine :: String -> Point
-    parseLine line = case words line of
-      [x, y] -> (read x, read y)
-      _ -> error "Parse error"
+  content <- B.readFile $ "../input/" ++ name ++ ".dat"
+  let (fptrw8, nw8) = BI.toForeignPtr0 content
+      arr = A.fromForeignPtrs (A.Z A.:. (nw8 `quot` 4)) (castForeignPtr fptrw8 :: ForeignPtr Int32) :: A.Array (A.Z A.:. Int) Int32
+      res = A.fromFunction (A.Z A.:. (nw8 `quot` 8)) (\(A.Z A.:. ix) -> (fromIntegral $ A.indexArray arr (A.Z A.:. 2*ix), fromIntegral $ A.indexArray arr (A.Z A.:. 2*ix+1))) 
+  return (name, res)
+
 
 testInput :: (String, A.Vector Point -> A.Vector Point) -> Input -> IO ()
 testInput (backend, f) (inputName, inputData) = do
   putStrLn $ backend ++ "/" ++ inputName
   putStrLn $ take 80 $ show $ f inputData
   putStrLn ""
+
+chunk :: Int -> [a] -> [[a]]
+chunk _ [] = []
+chunk i xs = let (f, r) = splitAt i xs in f : chunk i r
