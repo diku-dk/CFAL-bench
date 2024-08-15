f0de1774d5fda3ab4db65ff61de749b6fb2d5f54
thomas <thomas.koopman@ru.nl>
2024-08-15 11:13:51 +0200

    nbody: do not spawn threads more often than necessary, remove uneccesary accel buffer

--- nbody-naive/CPU_baselines/src/nbody.c
+++ nbody-naive/CPU_baselines/src/nbody.c
@@ -1,9 +1,11 @@
 #include <math.h>
 #include <omp.h>
 #include <stdio.h>
+#include <immintrin.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <sys/time.h>
+#include <immintrin.h>
 
 #define EPSILON2 0x1p-53
 
@@ -24,9 +26,9 @@ typedef struct {
 Points alloc_points(int n)
 {
     Points p;
-    p.x = malloc(n * sizeof(double));
-    p.y = malloc(n * sizeof(double));
-    p.z = malloc(n * sizeof(double));
+    p.x = aligned_alloc(64, n * sizeof(double));
+    p.y = aligned_alloc(64, n * sizeof(double));
+    p.z = aligned_alloc(64, n * sizeof(double));
     return p;
 }
 
@@ -55,51 +57,46 @@ double pow3(double x)
     return x * x * x;
 }
 
-/* accel[i] = sum_j m[j] (pos[i] - pos[j]) / ||pos[i] - pos[j]||^3 */
-/* 19 n^2 flops */
-void accelerateAll(Points accel, Points positions, double *masses, int n)
-{
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        double ax = 0.0;
-        double ay = 0.0;
-        double az = 0.0;
-        /* Loop body is 19 flops (assuming masses[j] / norm is computed
-         * only once) */
-        for (int j = 0; j < n; j++) {
-            double bufx = positions.x[j] - positions.x[i];
-            double bufy = positions.y[j] - positions.y[i];
-            double bufz = positions.z[j] - positions.z[i];
-            /* norm = ||positions[i] - positions[j]||^3 */
-            double norm = pow3(sqrt(bufx * bufx +
-                                    bufy * bufy +
-                                    bufz * bufz) + EPSILON2);
-            ax += bufx * masses[j] / norm;
-            ay += bufy * masses[j] / norm;
-            az += bufz * masses[j] / norm;
-        }
-        accel.x[i] = ax;
-        accel.y[i] = ay;
-        accel.z[i] = az;
-    }
-}
-
-/* Advances the n-bodies in place. accel is a buffer, does not need to be
- * initialized.
+/* Advances the n-bodies in place by iterations.
  * 19n^2 + 12n flops */
 void advance(Points positions, Points velocities, double *masses,
-             Points accel, double dt, int n)
+             double dt, int n, int iterations)
 {
-    accelerateAll(accel, positions, masses, n);
+    #pragma omp parallel
+    {
+        for (int t = 0; t < iterations; t++) {
+            /* omp for has implicit barrier at the end */
+            #pragma omp for
+            for (int i = 0; i < n; i++) {
+                double ax = 0.0;
+                double ay = 0.0;
+                double az = 0.0;
+                /* Loop body is 19 flops (assuming masses[j] / norm is computed
+                 * only once) */
+                for (int j = 0; j < n; j++) {
+                    double bufx = positions.x[j] - positions.x[i];
+                    double bufy = positions.y[j] - positions.y[i];
+                    double bufz = positions.z[j] - positions.z[i];
+                    /* norm = ||positions[i] - positions[j]||^3 */
+                    double norm = pow3(sqrt(bufx * bufx +
+                                            bufy * bufy +
+                                            bufz * bufz) + EPSILON2);
+                    ax += bufx * masses[j] / norm;
+                    ay += bufy * masses[j] / norm;
+                    az += bufz * masses[j] / norm;
+                }
+                velocities.x[i] += ax * dt;
+                velocities.y[i] += ay * dt;
+                velocities.z[i] += az * dt;
+            }
 
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        velocities.x[i] += accel.x[i] * dt;
-        velocities.y[i] += accel.y[i] * dt;
-        velocities.z[i] += accel.z[i] * dt;
-        positions.x[i] += velocities.x[i] * dt;
-        positions.y[i] += velocities.y[i] * dt;
-        positions.z[i] += velocities.z[i] * dt;
+            #pragma omp for
+            for (int i = 0; i < n; i++) {
+                positions.x[i] += velocities.x[i] * dt;
+                positions.y[i] += velocities.y[i] * dt;
+                positions.z[i] += velocities.z[i] * dt;
+            }
+        }
     }
 }
 
@@ -126,27 +123,26 @@ int main(int argc, char **argv)
 
     Points positions = alloc_points(n);
     Points velocities = alloc_points(n);
-    Points accel = alloc_points(n);
     double *masses = (double *)malloc(n * sizeof(double));
 
     init(positions, velocities, masses, n);
 
     struct timeval tv1, tv2;
     gettimeofday(&tv1, NULL);
-    for (int i = 0; i < iterations; i++) {
-        advance(positions, velocities, masses, accel, 0.01, n);
-    }
+    advance(positions, velocities, masses, 0.01, n, iterations);
     gettimeofday(&tv2, NULL);
     double duration = (double) (tv2.tv_usec - tv1.tv_usec) / 1e6 +
                       (double) (tv2.tv_sec - tv1.tv_sec);
 
+#ifdef DEBUG
     fprintf(stderr, "Bodies\n");
     for (int i = 0; i < n; i++) {
         fprintf(stderr, "(%.17e, %.17e, %.17e)\n", 
                     positions.x[i], positions.y[i], positions.z[i]);
     }
+#endif
 
-    fprintf(stderr, "Sequential nbody with %d bodies, %d iterations.\n"
+    fprintf(stderr, "Nbody with %d bodies, %d iterations.\n"
                     "This took %lfs.\n"
                     "Compute rate in Gflops/s: ",
                     n, iterations, duration);
@@ -155,7 +151,6 @@ int main(int argc, char **argv)
 
     free_points(positions);
     free_points(velocities);
-    free_points(accel);
     free(masses);
 
     return EXIT_SUCCESS;
