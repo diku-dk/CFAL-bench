cfeb3cce9f76b95cd70cfde7215fb282d83c635f
thomas <thomas.koopman@ru.nl>
2024-10-08 12:48:29 +0200

    Another way to break it. I am not sure what SaC and gcc do because the generated code is is a very large unrolled mess

--- LocVolCalib/baseline-FinPar/OrigCpp/VolCalibOrig.cpp
+++ LocVolCalib/baseline-FinPar/OrigCpp/VolCalibOrig.cpp
@@ -10,7 +10,8 @@ typedef double REAL;
 
 using namespace std;
 
-#define BREAK
+#define BREAK1
+//#define BREAK2
 
 // Macros for 2-dim array indexing
 #define Dx(i,j)       Dx[(i)*3 + j]
@@ -216,8 +217,9 @@ rollback(   const unsigned numX,
     //	explicit x
     for(j=0; j<numY; j++) {
         for(i=0; i<numX; i++) {
-#ifdef BREAK
-            /* Mathematically equivalent, but some values in U are off by
+#ifdef BREAK1
+            /* Pull ResultE(i, j) into the sum.
+             * Mathematically equivalent, but some values in U are off by
              * a factor 10. */
             if (0 < i) 
             U(j,i) += 0.5 * ResultE(i-1,j) * ( MuX(j,i)*Dx(i,0) + 0.5*VarX(j,i)*Dxx(i,0) );
@@ -227,15 +229,17 @@ rollback(   const unsigned numX,
             if (i < numX-1) 
             U(j,i) += 0.5 * ResultE(i+1,j) * ( MuX(j,i)*Dx(i,2) + 0.5*VarX(j,i)*Dxx(i,2) );
 #else
+    #ifdef BREAK2
+            /* Change order */
+    #else
             U(j,i) = dtInv * ResultE(i,j);
 
-            if (0 < i) 
-            U(j,i) += 0.5 * ResultE(i-1,j) * ( MuX(j,i)*Dx(i,0) + 0.5*VarX(j,i)*Dxx(i,0) );
 
             U(j,i) += 0.5 * ResultE(i,  j) * ( MuX(j,i)*Dx(i,1) + 0.5*VarX(j,i)*Dxx(i,1) );
 
             if (i < numX-1) 
             U(j,i) += 0.5 * ResultE(i+1,j) * ( MuX(j,i)*Dx(i,2) + 0.5*VarX(j,i)*Dxx(i,2) );
+    #endif
 #endif
         }
     }
