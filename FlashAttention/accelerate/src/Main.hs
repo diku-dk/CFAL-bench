c1b6aa1437bfe8b150d4fb731a10327250bba012
David van Balen <dpvanbalen@hotmail.com>
2024-10-24 11:51:52 +0200

    flashaccelerate

--- FlashAttention/accelerate/src/Main.hs
+++ FlashAttention/accelerate/src/Main.hs
@@ -9,31 +9,38 @@ import Criterion.Main
 import Naive
 import Flash_alg1
 import Criterion.Types (Benchmark(Benchmark))
+import Criterion.Measurement
+import Criterion.Measurement.Types (Measured(measTime))
 
 main :: IO ()
 main = do
-  -- check: these two values should be _very close_
+  -- -- check: these two values should be _very close_
   -- print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [8])
   -- print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [64])
   -- print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [1024])
-  -- print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [16384])
+  print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [16384])
 
-  -- higher values for `m` crash Accelerate for some reason, even though the formula claims that these three should be fine:
+  -- -- higher values for `m` crash Accelerate for some reason, even though the formula claims that these three should be fine:
   -- print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [32768])
   -- print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [65536])
   -- print $ CPU.runN check (A.fromList A.Z [512], A.fromList A.Z [64], A.fromList A.Z [131072])
 
-  defaultMain [backend "CPU" CPU.runN
+  -- defaultMain [
+  backend "CPU" CPU.runN
                 -- , backend "GPU" GPU.runN]
-                   ]
+                  --  ]
   where
     backend name runN
-      = bgroup name
-      $ map (testcase runN)
-      $ (,,) <$> [512] --, 1024, 2048, 4096, 8192, 16384]
-             <*> [64] --, 128]
-             <*> [8] --, 64, 512, 2048, 8192, 16384]
-    testcase runN (n,d,m) = bench ("n" ++ show n ++ ", d" ++ show d ++ ", m" ++ show m)
-      $ nf (runN totalProgram) ( A.fromList A.Z [n]
+      = do
+        print name
+        mapM_ (testcase runN)
+          $ concatMap (\(d,n) -> [(n,d,m) | m <- [16384]]) [(64,16384),(64,32768),(128,8192),(128,16384)]
+      -- $ (,,) <$> [512] --, 1024, 2048, 4096, 8192, 16384]
+      --        <*> [64] --, 128]
+      --        <*> [8] --, 64, 512, 2048, 8192, 16384]
+    testcase runN (n,d,m) = do
+      print ("n" ++ show n ++ ", d" ++ show d ++ ", m" ++ show m)
+      (measured, endtime) <- measure (nf (runN totalProgram) ( A.fromList A.Z [n]
                                , A.fromList A.Z [d]
-                               , A.fromList A.Z [m])
+                               , A.fromList A.Z [m])) 1
+      print  (secs $ measTime measured)
