b89e4f43124063307685ab063f7b85b400d7445b
Thomas Koopman <thomas.koopman@ru.nl>
2026-02-17 12:44:17 +0100

    Measure stddev convergence script for CPU

---
+++ nbody-naive/C/graph_stddev.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#SBATCH --account=csmpi
+#SBATCH --partition=csmpi_fpga_long
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:nvidia_a30:1
+#SBATCH --mem=64G
+#SBATCH --time=4:00:00
+#SBATCH --output=omp.out
+
+# No idea why this is necessary, something
+# with slurm and the FPGA
+export XILINX_XRT=/opt/xilinx/xrt
+
+if [ "$#" -ne 4 ]; then
+    printf 'Usage: %s N ITERATIONS RUNS OUT_DIR\n\n' "$0" >&2
+    printf '\tN: Number of bodies\n\n' >&2
+    printf '\tITERATIONS: Number of advancements in the benchmark\n\n' >&2
+    printf '\tRUNS: How often to run the benchmark\n\n' >&2
+    printf '\tOUT_DIR: Directory to store benchmark results\n\n' >&2
+    exit 1
+fi
+
+n="$1"
+iter="$2"
+runs="$3"
+outfile="$4/nbody_${n}_${iter}_stddev"
+mkdir -p "$4"
+
+make clean
+make -j
+
+printf 'n,stddev\n' > "${outfile}"
+
+printf '%d,' >> "${outfile}"
+{
+    i=1
+    while [ $i -le "$runs" ]
+    do
+        OMP_PLACES=cores numactl --interleave all ./nbody_omp "$n" "$iter"
+        i=$(( i + 1 ))
+    done
+} | awk '{
+           b = a + ($1 - a) / NR;
+           q += ($1 - a) * ($1 - b);
+           a = b;
+           printf "%d,%f\n", NR, sqrt(q / NR);
+         }' >> "${outfile}"
