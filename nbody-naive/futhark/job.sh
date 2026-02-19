080bcbc5314b6c1131079f4f48273f4476c72eac
Troels Henriksen <athas@sigkill.dk>
2026-02-19 21:48:04 +0100

    Add job.sh for nbody/futhark.

---
+++ nbody-naive/futhark/job.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#SBATCH --account=csmpi
+#SBATCH --partition=csmpi_fpga_short
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:nvidia_a30:1
+#SBATCH --mem=64G
+#SBATCH --time=0:10:00
+#SBATCH --output=nbody_baseline-cpu.out
+
+set -e
+
+rm -f *.runtimes
+
+futhark bench --backend=multicore nbody.fut --json nbody_cpu32.json
+../../util/futhark-json2runtimes.py nbody_cpu32.json nbody.fut n=1000 \
+                                    > nbody_futhark_cpu32_n1000.runtimes
+../../util/futhark-json2runtimes.py nbody_cpu32.json nbody.fut n=10000 \
+                                    > nbody_futhark_cpu32_n10000.runtimes
+../../util/futhark-json2runtimes.py nbody_cpu32.json nbody.fut n=100000 \
+                                    > nbody_futhark_cpu32_n100000.runtimes
+
+futhark bench --backend=multicore nbody.fut --json nbody_cpu1.json --pass-option=--num-threads=1
+../../util/futhark-json2runtimes.py nbody_cpu1.json nbody.fut n=1000 \
+                                    > nbody_futhark_cpu1_n1000.runtimes
+../../util/futhark-json2runtimes.py nbody_cpu1.json nbody.fut n=10000 \
+                                    > nbody_futhark_cpu1_n10000.runtimes
+../../util/futhark-json2runtimes.py nbody_cpu1.json nbody.fut n=100000 \
+                                    > nbody_futhark_cpu1_n100000.runtimes
+
+futhark bench --backend=cuda nbody.fut --json nbody_cuda.json
+../../util/futhark-json2runtimes.py nbody_cuda.json nbody.fut n=1000 \
+                                    > nbody_futhark_cuda_n1000.runtimes
+../../util/futhark-json2runtimes.py nbody_cuda.json nbody.fut n=10000 \
+                                    > nbody_futhark_cuda_n10000.runtimes
+../../util/futhark-json2runtimes.py nbody_cuda.json nbody.fut n=100000 \
+                                    > nbody_futhark_cuda_n100000.runtimes
