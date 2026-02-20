6bdad317236d0e7cc2322e8fccce7d0bc5904654
Thomas Koopman <thomas.koopman@ru.nl>
2026-02-20 13:35:19 +0100

    Quickhull script and Makefile

---
+++ quickhull/sac/bench_seq.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+#SBATCH --account=csmpi
+#SBATCH --partition=csmpi_fpga_long
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:nvidia_a30:1
+#SBATCH --mem=0
+#SBATCH --time=4:00:00
+#SBATCH --output=seq.out
+
+# No idea why this is necessary, something
+# with slurm and the FPGA
+export XILINX_XRT=/opt/xilinx/xrt
+
+if [ "$#" -ne 2 ]; then
+    printf 'Usage: %s RUNS OUT_DIR\n\n' "$0" >&2
+    exit 1
+fi
+
+runs="$1"
+outdir="$2"
+
+make input
+make seq
+mkdir -p "$outdir"
+
+bench()
+{
+    dataset="$1"
+    data=input/100M_"$dataset"
+
+    name=quickhull_"$dataset"
+
+    # Warmup
+    {
+        i=1
+        while [ $i -le 3 ]
+        do
+            /usr/bin/time -v numactl --interleave all ./bin/quickhull < "$data"
+            i=$(( i + 1 ))
+        done
+    }
+
+    {
+        {
+            i=1
+            while [ $i -le "$runs" ]
+            do
+                /usr/bin/time -v numactl --interleave all ./bin/quickhull < "$data"
+                i=$(( i + 1 ))
+            done
+        } | tee "${outdir}/${name}.raw" | \
+        awk '{
+                   b = a + ($1 - a) / NR;
+                   q += ($1 - a) * ($1 - b);
+                   a = b;
+                 } END {
+                   printf "%f,%f", a, sqrt(q / (NR - 1));
+                 }' > "${outdir}/${name}.csv"
+    } 2>&1 | \
+      grep "Maximum resident" | \
+      sed  's/^[^:]*:[ ]*//g' | \
+      awk '{print $1 * 1000}' | \
+      tee "${outdir}/${name}_mem.raw" | \
+      awk '{
+               b = a + ($1 - a) / NR;
+               q += ($1 - a) * ($1 - b);
+               a = b;
+             } END {
+               printf "%f,%f", a, sqrt(q / (NR - 1));
+             }' > "${outdir}/${name}_mem.csv"
+}
+
+bench circle
+bench rectangle
+bench quadratic
