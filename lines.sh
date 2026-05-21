5e13dfcef63d2a99a588b7d0f286fb94eebefad6
Tom Smeding <tom@tomsmeding.com>
2026-05-21 22:17:48 +0100

    lines.sh: accelerate: Count only actual algorithm code, not IO

--- lines.sh
+++ lines.sh
@@ -38,7 +38,7 @@ printf "%15s  Acc  APL DaCe--C/G  Fut  SAC  CPU   GPU\n" ""
 
 printf "%15s %4d %4d %4d %4d %4d %4d %4d %5d\n" \
        nbody \
-       $(sloc_accelerate nbody-naive/accelerate/nbody-naive/src/*.hs) \
+       $(sloc_accelerate nbody-naive/accelerate/nbody-naive/src/{Nbody,Physics}.hs) \
        $(sloc_apl nbody-naive/APL/nbody_naive_v4.apln) \
        $(sloc_dace nbody-naive/dace/nbody_dace_cpu_impl.py) \
        $(sloc_dace nbody-naive/dace/nbody_dace_gpu_impl.py) \
@@ -49,7 +49,7 @@ printf "%15s %4d %4d %4d %4d %4d %4d %4d %5d\n" \
 
 printf "%15s %4d %4d %4d %4s %4d %4d %4d %5d\n" \
        LocVolCalib \
-       $(sloc_accelerate LocVolCalib/accelerate/src/*.hs) \
+       $(sloc_accelerate LocVolCalib/accelerate/src/LocVolCalib.hs) \
        $(sloc_apl LocVolCalib/APL/finpar.apln) \
        $(sloc_dace LocVolCalib/dace/locvolcalib_dace.py) \
        "same" \
@@ -60,7 +60,7 @@ printf "%15s %4d %4d %4d %4s %4d %4d %4d %5d\n" \
 
 printf "%15s %4d %4d %4d %4s %4d %4d %4s %5d\n" \
        MG \
-       $(sloc_accelerate MG/accelerate/src/*.hs) \
+       $(sloc_accelerate MG/accelerate/src/MG.hs) \
        $(sloc_apl MG/APL/MG_k.apln) \
        $(sloc_dace MG/dace/mg_dace.py) \
        "same" \
@@ -72,7 +72,7 @@ printf "%15s %4d %4d %4d %4s %4d %4d %4s %5d\n" \
 
 printf "%15s %4d %4d %4s %4s %4d %4d %4d %5s\n" \
        QuickHull \
-       $(sloc_accelerate quickhull/accelerate/src/*.hs) \
+       $(sloc_accelerate quickhull/accelerate/src/Quickhull.hs) \
        $(sloc_apl quickhull/APL/quickhull_k.apln) \
        "---" \
        "---" \
@@ -83,7 +83,7 @@ printf "%15s %4d %4d %4s %4s %4d %4d %4d %5s\n" \
 
 printf "%15s %4d %4d %4d %4d %4d %4d %4d %5d\n" \
        FlashAttention \
-       $(sloc_accelerate FlashAttention/accelerate/src/*.hs) \
+       $(sloc_accelerate FlashAttention/accelerate/src/Flash_custom.hs) \
        $(sloc_apl FlashAttention/APL/flash_attention_k.apln) \
        $(sloc_dace FlashAttention/dace/flash_attention_dace_cpu.py) \
        $(sloc_dace FlashAttention/dace/flash_attention_dace_gpu.py) \
